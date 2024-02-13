54d6bba873a27e082f88f2f4c40054319bfbb154
Fabrício Rodrigues <638883@unicaipatinga.com.br>
2024-02-13 17:54:39 -0300

    layout update

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,11 @@
 <template>
 <body class="holder">
-    <div class="divs">
+    <div class="left-c">
       <div class="texto">Olá! Meu nome é Fabrício, sou um desenvolvedor web 
         junior apaixonado por VueJS. Estou pronto para contribuir com meus 
         conhecimentos e habilidades para o seu projeto.</div>
-        <RouterLink to="/portfolio" class="botao-saiba-mais">
+      <RouterLink to="/portfolio" class="botao-saiba-mais">
         SAIBA MAIS</RouterLink>
-      <div>1</div>
     </div>
    <img class="definitive" src=../assets/definetiveImage.svg>
 
@@ -26,13 +25,17 @@
   flex-direction: row;
   justify-content: space-around;
   align-itens: center;
-  height: 1000px;}
+  height: 1000px;
+}
 
-divs{
+.left-c{
   display:flex;
-  background-color: red;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: center;
+  align-itens: flex-end;
+  heigth: 200px;
+  width: 650px;
+  text-align: justify; 
 }
 .definitive{
   heigth: 467.47px;
@@ -40,23 +43,21 @@ divs{
 }
 
 .texto{
-  width: 677px;
-  heigth: 190px;
   font-family: Montserrat;
 }
 
 
 .botao-saiba-mais{
   background-color: #2AA8DB;
-  padding: 20px;
+  display:flex;
+  justify-content: center;
+  margin-top: 40px;
+  padding: 10px;
   border-radius: 32px;
   width: 144px;
-  height: 5px;
+  heigth: 5px;
+  color: white;
 }
 
-.definitive{
-  heigth: 467.47px;
-  width: 606px;
-}
  
 </style>
