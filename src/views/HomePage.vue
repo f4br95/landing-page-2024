b55eb1d6fd94edc2044795fed577bd55c5ca8898
Fabrício Rodrigues <638883@unicaipatinga.com.br>
2024-03-06 15:51:34 -0300

    changing <style> files plus using basic vue resources

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,69 +1,29 @@
 <template>
 <body class="holder">
-    <div class="left-c">
-      <div class="texto">Olá! Meu nome é Fabrício, sou um desenvolvedor web 
-        junior apaixonado por VueJS. Estou pronto para contribuir com meus 
-        conhecimentos e habilidades para o seu projeto.</div>
-      <RouterLink to="/portfolio" class="botao-saiba-mais">
+    <div class="holder-left">
+      <div class="holder-texto">
+        <p>{{descricao}}</p>
+
+      </div>
+      <RouterLink to="/portfolio" class="holder-botao-saiba-mais">
         SAIBA MAIS</RouterLink>
     </div>
-   <img class="definitive" src=../assets/definetiveImage.svg>
+   <img class="holder-img" src=../assets/definetiveImage.svg>
 
 
 
 </body>  
 </template>  
-    
 <script setup>
 
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 onMounted(() => {
-  console.log("mounted")
   document.title = "Landing Page"
 });
 
+const descricao = ref("Olá! Meu nome é Fabrício, sou um desenvolvedor junior apaixonado por VueJS. Estou pronto para contribuir com meus conhecimentos e habilidades para o seu projeto.");
 </script>
 
 <style>
-
-.holder{
-  display:flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-itens: center;
-  height: 1000px;
-}
-
-.left-c{
-  display:flex;
-  flex-direction: column;
-  justify-content: center;
-  align-itens: flex-end;
-  heigth: 200px;
-  width: 650px;
-  text-align: justify; 
-}
-.definitive{
-  heigth: 467.47px;
-  width: 606px;
-}
-
-.texto{
-  font-family: Montserrat;
-}
-
-
-.botao-saiba-mais{
-  background-color: #2AA8DB;
-  display:flex;
-  justify-content: center;
-  margin-top: 40px;
-  padding: 10px;
-  border-radius: 32px;
-  width: 144px;
-  heigth: 5px;
-  color: white;
-}
-
  
 </style>
