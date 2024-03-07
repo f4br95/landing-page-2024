20171f0c1f4792883a5965591029bfc983049868
Fabrício Rodrigues <638883@unicaipatinga.com.br>
2024-03-07 16:30:14 -0300

    replacing Homepage

---
+++ src/views/Homepage.vue
@@ -0,0 +1,22 @@
+<template>
+<body class="holder">
+    <div class="holder-left">
+      <div class="holder-texto">
+        <p>{{descricao}}</p>
+
+      </div>
+      <RouterLink to="/portfolio" class="holder-botao-saiba-mais">
+        SAIBA MAIS</RouterLink>
+    </div>
+   <img class="holder-img" src=../assets/definetiveImage.svg>
+
+</body>  
+</template>  
+<script setup>
+import {ref}  from 'vue'
+const descricao = ref("Olá! Meu nome é Fabrício, sou um desenvolvedor junior apaixonado por VueJS. Estou pronto para contribuir com meus conhecimentos e habilidades para o seu projeto.");
+</script>
+
+<style>
+ 
+</style>
